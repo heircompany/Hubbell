5be27c8711f31642106dd736eb5ad70ffc1c8920
Joe Grotto <joegrotto@heir.company>
2017-04-14 13:04:27 -0500

    heroku port added

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
-const port = process.env.PORT || 3000;
+
   database: 'mongodb://admin:admin@ds161400.mlab.com:61400/heroku_9k7pf22q',
-  port,
+  port: process.env.PORT || 3000
   secretKey: "puddles"
 }
