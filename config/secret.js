b77ba0a90abc8f2ae6473684084e04637194bc74
Joe Grotto <joegrotto@heir.company>
2017-04-14 13:13:04 -0500

    bug fix

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
 
   database: 'mongodb://admin:admin@ds161400.mlab.com:61400/heroku_9k7pf22q',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
   secretKey: "puddles"
 }
